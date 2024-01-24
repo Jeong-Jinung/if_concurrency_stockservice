9b695f010300ee0b19e5bc3df22a4e3da424455a
JeongJinUng <jinung.dev@gmail.com>
2024-01-24 21:53:04 +0900

    :sparkles: 문제점

--- src/test/java/org/example/stockservice/service/StockServiceTest.java
+++ src/test/java/org/example/stockservice/service/StockServiceTest.java
@@ -2,6 +2,11 @@ package org.example.stockservice.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 import org.aspectj.lang.annotation.Before;
 import org.example.stockservice.domain.Stock;
 import org.example.stockservice.repository.StockRepository;
@@ -37,7 +42,33 @@ class StockServiceTest {
         stockService.decrease(1L, 1L);
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99L, stock.getQuantity());
-
     }
+
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 레이스 컨디션이 발생하여 원하는 결과가 나오지 않는다.
+        // 하나의 스레드가 작업 완료된 후 다른 스레드의 접근을 허용하게 해주어야 한다.
+        assertEquals(0, stock.getQuantity());
+    }
+
+
 
 }
